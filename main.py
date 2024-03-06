9d2091799f5f871c2d538ab91e0ab2918548bc72
ccozort <ccozort@bcp.org>
2024-03-06 13:28:29 -0800

    moved cooldown class to utils

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ goals, rules, feedback, freedom, what the verb, and will it form a sentence
 import pygame as pg
 from settings import *
 from sprites import *
+from utils import *
 from random import randint
 import sys
 from os import path
@@ -20,32 +21,7 @@ from os import path
 # added this math function to round down the clock
 from math import floor
 
-# this 'cooldown' class is designed to help us control time
-class Cooldown():
-    # sets all properties to zero when instantiated...
-    def __init__(self):
-        self.current_time = 0
-        self.event_time = 0
-        self.delta = 0
-        self.cd = 0
-        # ticking ensures the timer is counting...
-    # must use ticking to count up or down
-    def ticking(self):
-        self.current_time = floor((pg.time.get_ticks())/1000)
-        self.delta = self.current_time - self.event_time
-    # resets event time to zero - cooldown reset
-    def get_cd(self):
-        return self.cd
-    def countdown(self, x):
-        x = x - self.delta
-        if x != None:
-            self.cd = x
-            return x
-    def event_reset(self):
-        self.event_time = floor((pg.time.get_ticks())/1000)
-    # sets current time
-    def timer(self):
-        self.current_time = floor((pg.time.get_ticks())/1000)
+
 
 
 # Define game class...
@@ -122,6 +98,7 @@ class Game:
         # tick the test timer
         if self.test_timer.countdown(5) == 0:
             self.test_timer.event_reset()
+            print("time's up")
         self.test_timer.ticking()
         self.all_sprites.update()
     
---
+++ utils.py
@@ -0,0 +1,28 @@
+# Utilities file
+
+# this 'cooldown' class is designed to help us control time
+class Cooldown():
+    # sets all properties to zero when instantiated...
+    def __init__(self):
+        self.current_time = 0
+        self.event_time = 0
+        self.delta = 0
+        self.cd = 0
+        # ticking ensures the timer is counting...
+    # must use ticking to count up or down
+    def ticking(self):
+        self.current_time = floor((pg.time.get_ticks())/1000)
+        self.delta = self.current_time - self.event_time
+    # resets event time to zero - cooldown reset
+    def get_cd(self):
+        return self.cd
+    def countdown(self, x):
+        x = x - self.delta
+        if x != None:
+            self.cd = x
+            return x
+    def event_reset(self):
+        self.event_time = floor((pg.time.get_ticks())/1000)
+    # sets current time
+    def timer(self):
+        self.current_time = floor((pg.time.get_ticks())/1000)
